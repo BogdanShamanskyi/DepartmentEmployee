9ec0cb547cc5876c6cddf3ec298f482b597bf6e6
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-03 23:52:47 +0300

    Rename employee position to position migration

--- database/migrations/2021_09_03_162132_create_positions_table.php
+++ database/migrations/2021_09_03_162132_create_positions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeePositionsTable extends Migration
+class CreatePositionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEmployeePositionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('employee_positions', function (Blueprint $table) {
+        Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
