afdc3aa907bc615c0d535c9a3c92b18f037beee4
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-05 00:59:59 +0300

    Created DepartmentFactory

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class DepartmentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Department::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->name;
+
+        return [
+            'name' => $name,
+            'description' => $this->faker->text(200),
+            'slug' => Str::slug($name)
+        ];
+    }
+}
