4fe2a685750dc616dc1c09695afc2476e2e2ede1
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-05 01:00:12 +0300

    Created PositionFactory

---
+++ database/factories/PositionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Position;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PositionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Position::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'abbreviation' => $this->faker->text(5),
+            'description' => $this->faker->text(200),
+        ];
+    }
+}
