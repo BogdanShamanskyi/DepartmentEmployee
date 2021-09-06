1c86f1a7eca7ca198f55e0da4641ba7aff7f05a2
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-06 22:44:43 +0300

    Changed route name from employee to employees

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
 
-Route::resource('employee', EmployeeController::class)->only('index', 'show');
+Route::resource('employees', EmployeeController::class)->only('index', 'show');
