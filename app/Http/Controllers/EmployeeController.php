bd88217a9ce9eabd34524fb9568c1c2e610f6e1b
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-06 22:15:14 +0300

    Filled index and show methods

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('employees');
+    }
+
+    public function show(Employee $employee)
+    {
+        return view('employee', $employee);
+    }
 }
