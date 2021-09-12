910dcf9481e4f2d77c3f375bb6151bfcab5bb66b
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-12 23:48:02 +0300

    Added resource route for department

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('index');
 
 Route::resource('employees', EmployeeController::class)->only('index', 'show');
+Route::resource('departments', DepartmentController::class)->only('index', 'show');
